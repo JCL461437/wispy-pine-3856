83fb13d11dfd4df7d9e4620b6b0f6ce051b1c86b
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-06 10:06:02 -0600

    Add faraday connection to the api in the welcome_controller{

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    #refactor to service object
+    connection = Faraday.new('https://api.nal.usda.gov') do |faraday|
+      # faraday.headers['X-Api-Key'] = Rails.application.credentials.agriculture[:key]
+      faraday.params['key'] = Rails.application.credentials.agriculture[:key]
+      faraday.adapter Faraday.default_adapter
   end
 end
